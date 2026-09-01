ffe782543a8c5077e9d5e0f84ef1979fa4ecbf77
iamnorbiato <35697840+iamnorbiato@users.noreply.github.com>
2026-08-31 21:07:00 -0300

    Add files via upload

---
+++ links.js
@@ -0,0 +1,9 @@
+const linksDoKit = {
+  memorial: "https://docs.google.com/document/d/1OC39t_LppcEUP-p6FwpdL_rMp2N1ofJjsoPIcRs59sg/edit?usp=sharing",
+  especificacao: "https://docs.google.com/document/d/1bTBq2yC6XjxYr2rULab-a8ai0bpXiQpx8VK5rMpNFNQ/edit?usp=sharing",
+  questionario: "https://forms.gle/we7BQiZvWnN8V7rg7",
+};
+
+document.querySelector("#link-memorial").href = linksDoKit.memorial;
+document.querySelector("#link-especificacao").href = linksDoKit.especificacao;
+document.querySelector("#link-questionario").href = linksDoKit.questionario;
